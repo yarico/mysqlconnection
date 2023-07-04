d27ba04b55ef7f166ed34b30ffa32d09f846b03a
yaric <yaric@yared>
2023-07-04 13:46:23 -0700

    sssssss

---
+++ ZZStudent/src/main/java/com/ZZStudentM/ZzStudentApplication.java
@@ -0,0 +1,13 @@
+package com.ZZStudentM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzStudentApplication.class, args);
+	}
+
+}
---
+++ ZZStudent/src/test/java/com/ZZStudentM/ZzStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZZStudentM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
